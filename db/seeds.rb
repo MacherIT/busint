7c058bf651de1cd17a6c474c56b47d2da6071fec
Bruno Calmels <brunocalmels@gmail.com>
2016-06-04 15:05:32 -0300

    Mejora view _deal y cambia seeds a valores de Macher.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,46 @@
 # -*- coding: utf-8 -*-
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name:  "Bruno Calmels",
+             email: "bruno.calmels@macherit.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+User.create!(name:  "Juan Pablo López",
+             email: "juan.lopez@macherit.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-users = User.order(:created_at).take(5)
+User.create!(name:  "Pedro Porma",
+             email: "pedro.porma@macherit.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
+
+#99.times do |n|
+#  name  = Faker::Name.name
+#  email = "example-#{n+1}@railstutorial.org"
+#  password = "password"
+#  User.create!(name:  name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password,
+#               activated: true,
+#               activated_at: Time.zone.now)
+#end
+
+users = User.order(:created_at).take(3)
 estados = ["Ganado", "Perdido", "En espera de respuesta"]
 productos = ["Web", "App", "Consultoría", "Neuromarketing"]
-50.times do
+10.times do
   empresa = Faker::Name.name
   estado = estados.sample
   fuente = Faker::Lorem.sentence(1)
