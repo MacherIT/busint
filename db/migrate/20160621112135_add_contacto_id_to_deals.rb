a0dfe32634f529eaae5a060225c7b4a94b3f9f8a
Bruno Calmels <brunocalmels@gmail.com>
2016-07-19 15:17:12 -0300

    Cambios para un crrecto migrate en posgres.

--- db/migrate/20160621112135_add_contacto_id_to_deals.rb
+++ db/migrate/20160621112135_add_contacto_id_to_deals.rb
@@ -1,8 +1,11 @@
 class AddContactoIdToDeals < ActiveRecord::Migration
   def change
-    add_column :deals, :contacto_id, :integer
-    add_foreign_key :deals, :contacto_id
-    add_index :deals, :contacto_id
+#    add_column :deals, :contacto_id, :integer
+#    add_foreign_key :deals, :contacto_id
+#    add_index :deals, :contacto_id
+    add_reference :deals, :contacto, index: true
+    add_foreign_key :deals, :contactos
+
     remove_column :deals, :empresa_id
   end
 end
