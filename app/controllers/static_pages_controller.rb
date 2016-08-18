0cc1f0464c53ee079ad530ff5fa040ee7bdb5293
Bruno Calmels <brunocalmels@gmail.com>
2016-08-18 11:55:48 -0300

    Pongo un calendario en inicio.html.erb para los usuarios logueados.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def inicio
+    if logged_in?
+      user_id = current_user.id
+      @eventos = Accion.where(user_id: user_id)
+    end
   end
 
   def form_visita
